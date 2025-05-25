3f769ec927184d581634fc452539b5275c726de7
DanyC2003 <danielacoronado0109@gmail.com>
2025-05-25 15:05:34 -0500

    dataweb

--- src/edu_pad/main.py
+++ src/edu_pad/main.py
@@ -1,10 +1,21 @@
+import os
 from edu_pad.dataweb import DataWeb
 
 def main():
     print("Ejecutando scraping de productos de Mercado Libre...")
     dw = DataWeb()
     dw.obtener_datos()
-    print("Proceso finalizado.")
+    if not dw.empty:
+        output_dir = "static/cvs"
+        os.makedirs(output_dir, exist_ok=True)
+        
+        cvs_path = os.path.join(output_dir, "data_web.csv")
+        dw.df.to_csv(cvs_path, index=False)
+
+        print(f"Datos exportados a '{cvs_path}' correctamente.")
+    else:
+        print("No se encontraron datos para exportar.")
+        
 
 if __name__ == "__main__":
     main()
